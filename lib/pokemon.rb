1fae45c6d6c902306b9db7e4c079617c97948115
Cody Frank <cody.frank30@gmail.com>
2019-06-29 00:00:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
-  attr_accessor :name, :type, :database
+  attr_accessor :name, :type, :db
   attr_reader :id
-  def initialize(id: nil, name:, type:, database)
-    @id, @name, @type, @database = id, name, type, database
+  def initialize(id: nil, name:, type:, db:)
+    @id, @name, @type, @database = id, name, type, db
   end
 end
