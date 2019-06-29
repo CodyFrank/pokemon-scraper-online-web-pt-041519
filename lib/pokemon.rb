645dfb01f8289f301623409e38bd3f4994a47d20
Cody Frank <cody.frank30@gmail.com>
2019-06-29 00:33:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,9 +20,11 @@ class Pokemon
     sql = <<-SQL
     SELECT * FROM pokemon WHERE id = ?
     SQL
-     db.execute(sql, id)#.map do |row|
-      # Pokemon.new(row)
-    # end
-    binding.pry
+     db.execute(sql, id).map do |row|
+       id = row[0]
+       name = row[1]
+       type = row[2]
+       Pokemon.new(id, name, type)
+    end
   end
 end
